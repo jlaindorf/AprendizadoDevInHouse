dd2c06b334a5164c1e374c79c818258d79595349
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-06-08 20:23:02 -0300

    Add files via upload

---
+++ aumentoDeSalario.js
@@ -0,0 +1,26 @@
+var cargo = prompt (`Digite seu Cargo para consultar o aumento de sário:
+                     101-Gerente
+                     102-Engenheiro
+                     103-Técnico`)
+var salario = Number(prompt('Agora digite seu salário atual'))
+
+
+ switch (cargo) {
+  case "101":{
+    console.log (`seu novo salário será ${salario + (salario *0.1)}`)
+    break;
+  }
+     case "102": {
+    console.log (`seu novo salário será ${salario + (salario * 0.2)}`)
+       break;
+  }
+    
+    case "103": {
+    console.log (`seu novo salário será ${salario + (salario * 0.3)}`)
+       break;
+    }
+    default :{
+      console.log (`seu novo salário será ${salario + (salario * 0.4)}`)
+       break;
+    }
+}

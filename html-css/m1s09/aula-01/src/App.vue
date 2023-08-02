92602b1a5c8fad2fd494bd4195d01d2e28b8b208
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-08-01 22:13:42 -0300

    inicio de vue,js

---
+++ html-css/m1s09/aula-01/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h1>Calculadora IMC</h1>
+      
+      <input type="number" placeholder="Peso"  v-model="peso"/>
+      <input type="number" placeholder="Altura" v-model="altura" />
+      <button v-on:click="calcular()">Calcular</button>
+      
+      <!-- AQUI VAI O RESULTADO -->
+      <span> {{ resultado }} </span>
+    </div>
+  </template>
+  
+  <script>
+    export default{
+
+        data(){
+            return{
+              peso : 0,
+              altura : 0,
+              resultado : "",
+             }
+
+        },
+        methods:{
+
+            calcular(){
+      if(this.peso == 0 || this.altura == 0){
+        alert("Os dados precisam ser maiores que 0")
+        return
+      }
+
+      this.resultado = (this.peso / (this.altura ** 2)).toFixed(2)
+    },
+        }
+
+    }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ html-css/m1s09/ex-par-impar/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Digite um número</h1>
+    
+    <input type="number" placeholder="Digite um número"  v-model="numero"/>
+   
+    <button v-on:click="calcular()">RESULTADO</button>
+    
+     </div>
+</template>
+
+<script>
+  export default{
+
+      data(){
+          return{
+           numero: 0,
+           }
+
+      },
+      methods:{
+
+          calcular(){
+            if (this.numero % 2 === 0) {
+        alert(`O número ${this.numero} é par.`);
+      } else {
+        alert(`O número ${this.numero} é impar.`);
+      }
+    },
+      }
+
+  }
+</script>
+
+<style>
+
+</style>

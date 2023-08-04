c3cad4e63f8a17edf8e52caa981db9e10373163e
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-08-03 21:38:49 -0300

    incluindo componente em vue

---
+++ html-css/m1s09/aula-03-components/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- utilizando o componente -->
+  <Card>
+    <template v-slot:header>
+      <h1>Titulo do meu card</h1>
+    </template>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, rem harum debitis animi ducimus expedita numquam autem nostrum, voluptatum suscipit dolorum unde vel eaque dolorem doloremque sunt. Ea, sit dicta.</p>
+    <template #footer>
+      <h6>Created by Bruno Costa</h6>
+    </template>
+  </Card>
+</template>
+<script >
+import Card from "./components/card.vue"
+
+export default{
+  components:{
+    Card
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ html-css/m1s09/aula-03-components/src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <slot name="header"></slot>
+        
+    </header>
+    <slot></slot>
+   <footer>
+    <slot name="footer"></slot>
+
+   </footer>
+  </template>
+
+<script></script>
+
+<style scoped>
+h1{
+    color: red;
+}
+
+</style>
